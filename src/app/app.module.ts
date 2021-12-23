85f23a6a24ba1f13996b9e36c11139d87bc39f76
Ömer Gedik <omerfarukgedikk@gmail.com>
2021-12-23 15:48:10 +0300

    generate component search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
