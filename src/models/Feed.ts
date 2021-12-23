249dd51086b6912c155584bee04a704488fcf664
Ömer Gedik <omerfarukgedikk@gmail.com>
2021-12-23 17:24:58 +0300

    generate Feed Model

---
+++ src/models/Feed.ts
@@ -0,0 +1,50 @@
+interface Image {
+  itemType: String,
+  itemId: String,
+  imageSize: String,
+  base64: String,
+  storeId: String
+}
+
+interface Location {
+  type: String,
+  coordinates: Number[]
+}
+
+interface GeoLocation {
+  approve: Boolean,
+  latitude: String,
+  longitude: String
+}
+
+interface WorkingHours {
+  day: Number,
+  open: String,
+  close: String,
+  closed: Boolean
+}
+
+interface StoreInfo {
+  id: String,
+  geoLocation: GeoLocation,
+  userPoint: Number,
+  workingHours: WorkingHours,
+  status: String,
+  rate: DoubleRange,
+  minOrderPrice: DoubleRange
+}
+
+interface Feed {
+  id: String,
+  title: String,
+  text: String,
+  type: String,
+  images: Image[],
+  location: Location,
+  isDinner: Boolean,
+  isDelivery: Boolean,
+  storeInfo: StoreInfo,
+  categoryId: String
+}
+
+export type { Feed }
