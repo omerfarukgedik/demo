aebf8ce45fc9987096636b4df625ab96859115e5
Ömer Gedik <omerfarukgedikk@gmail.com>
2021-12-24 14:16:20 +0300

    fix tslint

--- src/app/services/get-feed.service.ts
+++ src/app/services/get-feed.service.ts
@@ -10,7 +10,7 @@ export class GetFeedService {
 
   constructor(private http: HttpClient) { }
 
-  getFeed(limit: number = 5, skip: number = 0, latitude: number = 0, longitude: number = 0): Observable<Feed[]> {
+  getFeed(limit: number = 10, skip: number = 0, latitude: number = 0, longitude: number = 0): Observable<any> {
     const headers = { 'apiKey': 'bW9jay04ODc3NTU2NjExMjEyNGZmZmZmZmJ2' };
     const body = { limit, skip, latitude, longitude };
 
